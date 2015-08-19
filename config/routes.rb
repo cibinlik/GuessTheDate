631678ec511f4131f3d23d2f39a8a9bf9f542f3e
cibinlik <cibinlik@gmail.com>
2015-08-19 02:55:36 +0000

    precompiled. welcome page created. cards added.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'guesses#year'
+  root 'guesses#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
